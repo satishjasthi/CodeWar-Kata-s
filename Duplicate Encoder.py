9db50a052ce1723620ad35f9e2d0d3b009ab6c15
satishjasthi <satish27may@gmail.com>
2016-12-18 21:41:36 +0530

    Fixed issue with the test case ((@,to obtain an output ))((

--- Duplicate Encoder.py
+++ Duplicate Encoder.py
@@ -1,17 +1,21 @@
-
-def duplicate_encoder(word):
-	l1 = list('recede') # converting string to list of individual characters
-	for index in range(len(l1)): #gathering and replacing the indexes of repeated elements of the list		
+def duplicate_encode(word):
+	l1 = list(word.lower())
+	for index in range(len(l1)):		
 		list_of_duplicates_for_item = [dup_index for dup_index, item in enumerate(l1) if item == l1[index] and l1.count(l1[index]) > 1]				
 		for dup_index in list_of_duplicates_for_item: 
 			l1[dup_index] = ')'
 
-	for i in range(len(l1)):#gathering and replacing the indexes of non-repeated elements of the list
+	for i in range(len(l1)):
 		if l1.count(l1[i]) <= 1:
-			l1[i] = '('
+			if l1[i] == '@':
+				l1[i] = '(('
+			if l1[i] == '(':
+				l1[i] = ')'
+			if l1[i] != '@':
+				l1[i] = '('
 
 
-	word = ''.join(l1);#joining the list of elements from list into a string
+	word = ''.join(l1);
 	return word
 
 
