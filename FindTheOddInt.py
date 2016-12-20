cb7fdd9e6457225b410bde1eddf20dd18f907e84
satishjasthi <satish27may@gmail.com>
2016-12-20 18:49:06 +0530

    Add other amazing solutions

--- FindTheOddInt.py
+++ FindTheOddInt.py
@@ -6,4 +6,20 @@
 def find_it(l):
 	return([number for number in l if (l.count(number)%2 != 0)][0])
 
-a = find_it([1,1,5,3,3,6,5,3,6,6,6]);print(a)
+#a = find_it([1,1,5,3,3,6,5,3,6,6,6]);print(a)
+
+#other amazing solutions:
+
+import operator
+
+def find_it(xs):
+    return reduce(operator.xor, xs)
+
+from collections import Counter
+def find_it(l):
+    return [k for k, v in Counter(l).items() if v % 2 != 0][0]
+
+def find_it(seq):
+    for i in seq:
+        if seq.count(i)%2!=0:
+            return i
